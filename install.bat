37f343cdb133322149d2b303c7c37e3b940d5144
Bastien <bastien_maurice@hotmail.com>
2018-10-10 21:04:54 +0200

    Création du script d'installation des prérequis

---
+++ install.bat
@@ -0,0 +1,16 @@
+@echo off
+echo.
+echo Installation de Numpy
+conda install numpy
+echo.
+echo Installation de Pillow
+conda install pillow
+echo.
+echo Installation de TQDM
+conda install tqdm
+echo.
+echo Creation du dossier DATASET
+mkdir dataset
+echo.
+echo Creation du dossier NUMPY
+mkdir numpy
